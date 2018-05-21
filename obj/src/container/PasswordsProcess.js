e7ef661d922b71ee01fb76586171b4b45bd2615d
pipdeveloper <pipdevs@gmail.com>
2018-05-21 12:38:58 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/PasswordsProcess.js
+++ obj/src/container/PasswordsProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const PasswordsServiceFactory_1 = require("../build/PasswordsServiceFactory");
 class PasswordsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("passwords", "User passwords microservice");
         this._factories.add(new PasswordsServiceFactory_1.PasswordsServiceFactory());
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.PasswordsProcess = PasswordsProcess;
--- obj/src/services/version1/PasswordsHttpServiceV1.js
+++ obj/src/services/version1/PasswordsHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class PasswordsHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('passwords');
+        super('v1/passwords');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-passwords', 'controller', 'default', '*', '1.0'));
     }
 }
