77e93543f623f4da67c09c4fcff1a02b684253e5
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:53:04 +0300

    added DefaultRpcFactory

--- obj/src/container/PasswordsProcess.js
+++ obj/src/container/PasswordsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const PasswordsServiceFactory_1 = require("../build/PasswordsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class PasswordsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("passwords", "User passwords microservice");
         this._factories.add(new PasswordsServiceFactory_1.PasswordsServiceFactory());
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory());
     }
 }
 exports.PasswordsProcess = PasswordsProcess;
--- src/container/PasswordsProcess.ts
+++ src/container/PasswordsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { PasswordsServiceFactory } from '../build/PasswordsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class PasswordsProcess extends ProcessContainer {
 
     public constructor() {
         super("passwords", "User passwords microservice");
         this._factories.add(new PasswordsServiceFactory());
+        this._factories.add(new DefaultRpcFactory());
     }
 
 }
