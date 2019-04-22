fe6e467ca7e0dffee49a5960e3195860600209b4
pipdeveloper <pipdevs@gmail.com>
2019-04-22 11:14:26 +0000

    project build by Travis CI [skip ci]

--- obj/src/index.js
+++ obj/src/index.js
@@ -9,5 +9,5 @@ __export(require("./logic"));
 __export(require("./services/version1"));
 __export(require("./build"));
 __export(require("./container"));
-//module.exports = require('./container/PasswordsSenecaPlugin');
+//module.exports = require('./container/PasswordsSenecaPlugin'); 
 //# sourceMappingURL=index.js.map
